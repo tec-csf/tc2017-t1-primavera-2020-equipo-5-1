17dd53623d19284dd4ad59cde2113b9301aa1309
Christian Dalma <49529715+Chrisdal97@users.noreply.github.com>
2020-03-10 15:24:59 -0600

    Update polinome.cpp

--- polinome.cpp
+++ polinome.cpp
@@ -1,3 +1,25 @@
+/*
+ * This file is part of {{ tc2017-t1-primavera-2020-equipo-5-1 }}.
+ *
+ * Developed for Diseño, Análisis de Algoritmos
+ * This product includes software developed by the LSST Project
+ * (https://www.lsst.org).
+ * See the COPYRIGHT file at the top-level directory of this distribution
+ * for details of code ownership.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 
 #include <iostream> 
 #include <cstring>
